46b442c821c718bd8c010a7b9db7e88ad8cd8d26
Ismail El Azzouzi <isel-azz@c2r1p7.1337.ma>
2024-09-22 08:54:39 +0100

    tokenization == done | tokenization->next == parsing aka ASTs

--- tokenize.c
+++ tokenize.c
@@ -13,17 +13,52 @@ t_token	*tokenize_input(char *input)
 			found_special_char(&input, &tokens);
 		else
 			found_word(&input, &tokens);
-		input++;
 	}
 	return (tokens);
 }
 
 void	found_special_char(char **input, t_token **tokens)
 {
-	
+	if (**input == '>')
+	{
+		if (*(*input + 1) == '>')
+		{
+			add_token(tokens, generate_token(REDIRECTION_APPEND, ">>"));
+			(*input)++;
+		}
+		else
+			add_token(tokens, generate_token(REDIRECTION_OUT, ">"));
+	}
+	else if (**input == '<')
+	{
+		if (*(*input + 1) == '<')
+		{
+			add_token(tokens, generate_token(HEREDOC, "<<"));
+			(*input)++;
+		}
+		else
+			add_token(tokens, generate_token(REDIRECTION_IN, "<"));
+	}
+	else if (**input == '|')
+		add_token(tokens, generate_token(PIPE, "|"));
+	(*input)++;
 }
 
 void	found_word(char **input, t_token **tokens)
 {
-	
+	char	*start;
+	char	quote;
+	int		quoted;
+
+	quote = '\0';
+	quoted = 0;
+	start = *input;
+	while (**input)
+	{
+		quote_status(**input, &quoted, &quote);
+		if (!quoted && ft_strchr(" \t\n><|", **input))
+			break;
+		(*input)++;
+	}
+	add_word_token(&start, input, tokens);
 }
